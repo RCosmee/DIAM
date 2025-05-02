1147c25dd2d2d0aaf4e1a65d2c49b94ad13ae32f
Ricardofixe <ricardoisidro28@gmail.com>
2025-05-02 18:10:26 +0100

    Mensagens e Database

--- projeto/sitepr/votacao/models.py
+++ projeto/sitepr/votacao/models.py
@@ -9,3 +9,21 @@ class Opcao(models.Model):
     questao = models.ForeignKey(Questao, on_delete=models.CASCADE)
     opcao_texto = models.CharField(max_length=200)
     votos = models.IntegerField(default=0)
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    avatar = models.URLField()
+    status = models.CharField(max_length=10, choices=['Online','Offline'])
+
+    def __str__(self):
+        return self.name
+
+class Message(models.Model):
+    group = models.ForeignKey(Group, related_name='messages', on_delete=models.CASCADE)
+    sender = models.CharField(max_length=100)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.sender}: {self.content[:20]}'
