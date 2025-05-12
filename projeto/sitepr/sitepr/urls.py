b13ea49234c7da4e9f470662b871a105b48f5d20
Rodrigo Santos <rodrigocosme999@gmail.com>
2025-05-12 20:45:59 +0100

    users

--- projeto/sitepr/sitepr/urls.py
+++ projeto/sitepr/sitepr/urls.py
@@ -1,7 +1,14 @@
-from django.urls import include, path 
 from django.contrib import admin
+from django.urls import path, include
 
+<<<<<<< Updated upstream
 urlpatterns = [ 
     path('api/', include('database.urls')), 
     path('admin/', admin.site.urls), 
 ] 
+=======
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('users.urls')),  # <-- ISTO É IMPORTANTE!
+]
+>>>>>>> Stashed changes
