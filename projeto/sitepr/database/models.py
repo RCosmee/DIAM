3d7cf16de962a86ed051b24bb5032dd8a96bece4
Ricardofixe <ricardoisidro28@gmail.com>
2025-05-15 17:08:58 +0100

    meh mensagens+user

--- projeto/sitepr/database/models.py
+++ projeto/sitepr/database/models.py
@@ -1,21 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Users e grupos
 class Chat(models.Model):
-    name = models.CharField(max_length=100, unique=True)
-    avatar = models.URLField()
-    status = models.CharField(max_length=10, choices=[('online', 'Online'), ('offline', 'Offline')])
-
-
+    name = models.CharField(max_length=100, unique=True, blank=True, null=True)
+    avatar = models.URLField(blank=True, null=True)  # avatar do grupo
+    participants = models.ManyToManyField(User, related_name='chats')  # lista de usuários
     def __str__(self):
-        return self.name
+        return self.name or "Chat sem nome"
 
 
 class Message(models.Model):
     chat = models.ForeignKey(Chat, related_name='messages', on_delete=models.CASCADE)
-    sender = models.CharField()
+    sender = models.ForeignKey(User, related_name='sent_messages', on_delete=models.CASCADE)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'{self.sender}: {self.content[:20]}'
+        return f'{self.sender.username}: {self.content[:20]}'
