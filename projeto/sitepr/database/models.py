d3a53ee1cd00d89ba6ea3ea0693739124d38393c
Rodrigo Santos <rodrigocosme999@gmail.com>
2025-05-17 22:47:03 +0100

    a

--- projeto/sitepr/database/models.py
+++ projeto/sitepr/database/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.postgres.fields import ArrayField  
+from django.db.models import JSONField 
 
 class Chat(models.Model):
-    name = models.CharField(max_length=100, unique=True, blank=True, null=True)
-    avatar = models.URLField(blank=True, null=True)  # avatar do grupo
-    participants = models.ManyToManyField(User, related_name='chats')  # lista de usuários
+    name = models.CharField(max_length=100, blank=True, null=True)
+    avatar = models.CharField(blank=True, null=True)  # avatar do grupo
+    participants = JSONField(default=list) 
 
     def __str__(self):
         return self.name or "Chat sem nome"
@@ -12,11 +14,9 @@ class Chat(models.Model):
 
 class Message(models.Model):
     chat = models.ForeignKey(Chat, related_name='messages', on_delete=models.CASCADE)
-    sender = models.ForeignKey(User, related_name='sent_messages', on_delete=models.CASCADE)
+    sender = models.IntegerField()  # id do usuário que enviou a mensagem
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-
-        return f'{self.sender.username}: {self.content[:20]}'
-
+        return f'User {self.sender}: {self.content[:20]}'
